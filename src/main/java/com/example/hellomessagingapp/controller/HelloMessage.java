b22c6d4901cc2aa913275dee1144aacea1162efd
Naman <cool.naman.ajmera@gmail.com>
2021-06-16 13:01:10 +0530

    Added: Use the GetMapping using parameter.

--- src/main/java/com/example/hellomessagingapp/controller/HelloMessage.java
+++ src/main/java/com/example/hellomessagingapp/controller/HelloMessage.java
@@ -1,19 +1,22 @@
 package com.example.hellomessagingapp.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/hello")
 public class HelloMessage {
     @RequestMapping(value = {"", "/", "/home"})
-    public String sayHello(){
+    public String sayHello() {
         return "Hello Naman";
     }
 
     @RequestMapping(value = {"/query", ""}, method = RequestMethod.GET)
     public String sayHello(@RequestParam(value = "fName") String fName, @RequestParam(value = "lName") String lName) {
         return "Hello " + fName + " " + lName + "!";
-    }}
+    }
+
+    @GetMapping("/param/{name}")
+    public String sayHelloParam(@PathVariable String name) {
+        return "Hello " + name + "!";
+    }
+}
