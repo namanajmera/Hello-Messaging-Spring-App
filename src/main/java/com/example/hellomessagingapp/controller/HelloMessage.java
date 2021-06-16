b5c2a42233551eba7b27b12b475814a5d78e84f3
Naman <cool.naman.ajmera@gmail.com>
2021-06-16 12:55:21 +0530

    Added: Add the GET Method to get the result.

--- src/main/java/com/example/hellomessagingapp/controller/HelloMessage.java
+++ src/main/java/com/example/hellomessagingapp/controller/HelloMessage.java
@@ -1,6 +1,8 @@
 package com.example.hellomessagingapp.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,4 +12,8 @@ public class HelloMessage {
     public String sayHello(){
         return "Hello Naman";
     }
-}
+
+    @RequestMapping(value = {"/query", ""}, method = RequestMethod.GET)
+    public String sayHello(@RequestParam(value = "fName") String fName, @RequestParam(value = "lName") String lName) {
+        return "Hello " + fName + " " + lName + "!";
+    }}
